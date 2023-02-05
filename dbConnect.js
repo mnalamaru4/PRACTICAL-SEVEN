36bf7c8bee3e8b25a52e0fadf11cb8904ce7ae56
mnalamaru4 <s222315268@deakin.edu.au>
2023-02-06 01:38:43 +1100

    updatede

--- dbConnect.js
+++ dbConnect.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
-const MongoClient = require('mongodb'). MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
-const uri = 'mongodb+srv://Mahidhar:9989@cluster0.iu84sbl.mongodb.net/?retryWrites=true&w=majority'
-const client = new MongoClient(uri, {useNewUrlParser: true})
+const uri =
+  "mongodb+srv://Mahidhar:9989@cluster0.iu84sbl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-client.connect ((err,db) => {
-    if(!err) {
-        console.log('MongoDB Connected')
-    }
-    else {
-        console.log("DB Error: ", err);
-        process.exit(1);
-    }
-})
+client.connect((err, db) => {
+  projectcollection = client.db().collection("projects");
+  if (!err) {
+    console.log("MongoDB Connected");
+  } else {
+    console.log("DB Error: ", err);
+    process.exit(1);
+  }
+});
 module.exports = client;
